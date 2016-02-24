b771c5f100318e0867590fd8a7eb0e1faed409d9
Jetus <jetuso@gmail.com>
2016-02-24 07:50:28 +0200

    Very raw version

--- Exchange Server Integration/ImportContacts/ImportContacts/Default.aspx.cs
+++ Exchange Server Integration/ImportContacts/ImportContacts/Default.aspx.cs
@@ -4,11 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.Exchange.WebServices.Data;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!Page.IsPostBack)
+            DataInitialization();
+    }
 
+    private void DataInitialization()
+    {
+        var service = new ExchangeService(ExchangeVersion.Exchange2013);
+        service.Credentials = new WebCredentials("user1@contoso.com", "password");
+        service.TraceEnabled = true;
+        service.TraceFlags = TraceFlags.All;
+        service.Url = new Uri("https://demo.atemiscloud.com/autodiscover/autodiscover");
+        service.AutodiscoverUrl("user1@contoso.com", RedirectionUrlValidationCallback);
+
+        EmailMessage email = new EmailMessage(service);
+        email.ToRecipients.Add("user1@contoso.com");
+        email.Subject = "HelloWorld";
+        email.Body = new MessageBody("This is the first email I've sent by using the EWS Managed API");
+        email.Send();
+    }
+
+    private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+    {
+        // The default for the validation callback is to reject the URL.
+        bool result = false;
+
+        Uri redirectionUri = new Uri(redirectionUrl);
+
+        // Validate the contents of the redirection URL. In this simple validation
+        // callback, the redirection URL is considered valid if it is using HTTPS
+        // to encrypt the authentication credentials. 
+        if (redirectionUri.Scheme == "https")
+        {
+            result = true;
+        }
+        return result;
     }
 }
