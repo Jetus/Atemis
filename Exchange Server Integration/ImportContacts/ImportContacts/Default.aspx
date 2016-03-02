1f96e9f79d20343823342680cc66788effffa746
Jetus <jetuso@gmail.com>
2016-03-03 00:44:54 +0200

    2nd push

--- Exchange Server Integration/ImportContacts/ImportContacts/Default.aspx
+++ Exchange Server Integration/ImportContacts/ImportContacts/Default.aspx
@@ -9,7 +9,16 @@
 <body>
     <form id="form1" runat="server">
     <div>
-        Hello
+        <dx:ASPxGridView runat="server" ID="gridData" AutoGenerateColumns="false" OnBeforePerformDataSelect="gridData_BeforePerformDataSelect">
+            <Columns>
+                <dx:GridViewDataTextColumn Caption="From" FieldName="From">
+                </dx:GridViewDataTextColumn>
+                <dx:GridViewDataTextColumn Caption="To" FieldName="Recipients">
+                </dx:GridViewDataTextColumn>
+                <dx:GridViewDataTextColumn Caption="Subject" FieldName="Subject">
+                </dx:GridViewDataTextColumn>
+            </Columns>
+        </dx:ASPxGridView>
     </div>
     </form>
 </body>
--- Exchange Server Integration/ImportContacts/ImportContacts/Default.aspx.cs
+++ Exchange Server Integration/ImportContacts/ImportContacts/Default.aspx.cs
@@ -9,35 +9,103 @@ using System.Net;
 
 public partial class _Default : System.Web.UI.Page
 {
+    private List<MailItem> InboxData
+    {
+        get { return Session["_InboxDataList"] as List<MailItem>; }
+        set { Session["_InboxDataList"] = value; }
+    }
+
+    private ExchangeService EWService
+    {
+        get { return Session["_EWService"] as ExchangeService; }
+        set { Session["_EWService"] = value; }
+    }
+
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!Page.IsPostBack)
-            DataInitialization();
+        {
+            ConnectService();
+            //LoadInboxData();
+            LoadContactsData();
+            gridData.DataBind();
+        }
     }
 
-    private void DataInitialization()
+    private void ConnectService()
     {
-        var service = new ExchangeService(ExchangeVersion.Exchange2013);
-
-        service.Credentials = new WebCredentials("ajaubert.sequinv", "Exchange.26!", "hasolution");
-        service.TraceEnabled = true;
-        service.TraceFlags = TraceFlags.All;
-        //service.Url = new Uri("https://demo.atemiscloud.com/autodiscover/autodiscover");
-        service.AutodiscoverUrl("agathejaubert@sequoiasinvestment.fr", RedirectionUrlValidationCallback);
-
-        //EmailMessage email = new EmailMessage(service);
-        //email.ToRecipients.Add("agathejaubert@sequoiasinvestment.fr");
-        //email.Subject = "HelloWorld";
-        //email.Body = new MessageBody("This is the first email I've sent by using the EWS Managed API");
-        //email.Send();
+        EWService = new ExchangeService(ExchangeVersion.Exchange2013);
+        EWService.Credentials = new WebCredentials("lchapelle", "Lud0v1351C*", "Achat-solution");
+        EWService.TraceEnabled = true;
+        EWService.TraceFlags = TraceFlags.All;
+        EWService.Url = new Uri("https://mail.achat-solution.fr/EWS/Exchange.asmx");
     }
 
-    private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+    private void LoadInboxData()
     {
-        bool redirectionValidated = false;
-        if (redirectionUrl.Equals("https://mailconfig.ovh.net/autodiscover/autodiscover.xml"))
-            redirectionValidated = true;
- 
-        return redirectionValidated;
+
+
+        FindItemsResults<Item> findResults = EWService.FindItems(WellKnownFolderName.Inbox, new ItemView(128));
+
+        ServiceResponseCollection<GetItemResponse> items =
+            EWService.BindToItems(findResults.Select(item => item.Id), new PropertySet(BasePropertySet.FirstClassProperties, EmailMessageSchema.From, EmailMessageSchema.ToRecipients));
+        InboxData = items.Select(item =>
+            {
+                return new MailItem()
+                {
+                    From = ((Microsoft.Exchange.WebServices.Data.EmailAddress)item.Item[EmailMessageSchema.From]).Address,
+                    Recipients = string.Join(", ", ((Microsoft.Exchange.WebServices.Data.EmailAddressCollection)item.Item[EmailMessageSchema.ToRecipients]).Select(recipient => recipient.Address).ToArray()),
+                    Subject = item.Item.Subject,
+                    Body = item.Item.Body.ToString(),
+                };
+            }).ToList();        
+    }
+
+    private void LoadContactsData()
+    {
+        // Get the number of items in the Contacts folder.
+        ContactsFolder contactsfolder = ContactsFolder.Bind(EWService, WellKnownFolderName.Contacts);
+
+        // Set the number of items to the number of items in the Contacts folder or 50, whichever is smaller.
+        int numItems = contactsfolder.TotalCount < 500 ? contactsfolder.TotalCount : 500;
+
+        // Instantiate the item view with the number of items to retrieve from the Contacts folder.
+        ItemView view = new ItemView(numItems);
+
+        // To keep the request smaller, request only the display name property.
+        view.PropertySet = new PropertySet(BasePropertySet.IdOnly, ContactSchema.DisplayName);
+
+        // Retrieve the items in the Contacts folder that have the properties that you selected.
+        FindItemsResults<Item> contactItems = EWService.FindItems(WellKnownFolderName.Contacts, view);
+
+        InboxData = contactItems.Where(x => x is Contact).Select(item =>
+            new MailItem()
+            {
+                From = (item as Contact).DisplayName
+            }).ToList();
+
+        // Display the list of contacts. 
+        //foreach (Item item in contactItems)
+        //{
+        //    if (item is Contact)
+        //    {
+        //        Contact contact = item as Contact;
+        //        Console.WriteLine(contact.DisplayName);
+        //    }
+        //}
+    }
+
+    // Simplified mail item
+    public class MailItem
+    {
+        public string From { get; set; }
+        public string Recipients { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+
+    protected void gridData_BeforePerformDataSelect(object sender, EventArgs e)
+    {
+        gridData.DataSource = InboxData;
     }
 }
